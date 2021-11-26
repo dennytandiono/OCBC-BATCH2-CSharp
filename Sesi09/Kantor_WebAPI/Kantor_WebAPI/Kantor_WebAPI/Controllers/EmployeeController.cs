ea4ee87eb80a452d48f6f47225782bb2a8d2425a
denny tandiono <denny11tandiono@gmail.com>
2021-11-26 13:22:04 +0700

    push sesi 09

---
+++ Sesi09/Kantor_WebAPI/Kantor_WebAPI/Kantor_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Kantor_WebAPI.Models;
+
+namespace Kantor_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet(Name = "GetEmployee")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            return _context.GetAllEmployee();
+        }
+
+        [HttpGet("{id}", Name = "GetEmployeeById")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItem(String id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            return _context.GetEmployee(id);
+        }
+    }
+}
